66817e25e77b9e9e3256ade66c8ec165887c4abc
Dev Sathwik P <82132397+Sathwik3010@users.noreply.github.com>
2025-12-11 11:13:47 +0530

    Custom class Object

---
+++ Temp.java
@@ -0,0 +1,16 @@
+package com.codegnan.map;
+
+public class Temp {
+	int i;
+
+	public Temp(int i) {
+		super();
+		this.i = i;
+	}
+
+	@Override
+	public String toString() {
+		return "i";
+	}
+	
+}
