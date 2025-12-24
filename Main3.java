dee6adc6d780fc7fcb34d9f46d34ebdab2b9b6e0
Dev Sathwik P <82132397+Sathwik3010@users.noreply.github.com>
2025-12-24 15:13:08 +0530

    Linked HashMap

---
+++ Main3.java
@@ -0,0 +1,27 @@
+package com.codegnan.collectionFramework;
+
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		LinkedHashMap<Integer, String> map = new LinkedHashMap<>();
+		
+		for(int i=0; i<n; i++) {
+			int key = sc.nextInt();
+			String value = sc.next();
+			map.put(key, value);
+		}
+		
+		for(String val: map.values()) {
+			System.out.print(val+" ");
+		}
+		sc.close();
+	}
+
+}
